daf9a7af21790425698d2c1b81532b9dfceed2cf
EugenS21 <Rzl7kxJJ^%3QL^D70zC2>
2019-01-22 19:17:14 +0200

    First commit from PC

---
+++ src/main/java/com/marksmanagement/marksmanagement/MarksmanagementApplication.java
@@ -0,0 +1,14 @@
+package com.marksmanagement.marksmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarksmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarksmanagementApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/marksmanagement/marksmanagement/MarksmanagementApplicationTests.java
@@ -0,0 +1,17 @@
+package com.marksmanagement.marksmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarksmanagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
