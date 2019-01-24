b9de6fd771ec78a1a3a25caecc2d43f307f0856a
EugenS21 <Rzl7kxJJ^%3QL^D70zC2>
2019-01-24 17:27:41 +0200

    adding functionality to inform user about data it has beed introduced in the database

--- src/main/java/com/marksmanagement/marksmanagement/controllers/LessonsController.java
+++ src/main/java/com/marksmanagement/marksmanagement/controllers/LessonsController.java
@@ -1,5 +1,6 @@
 package com.marksmanagement.marksmanagement.controllers;
 import com.marksmanagement.marksmanagement.services.LessonService;
+import com.marksmanagement.marksmanagement.services.MarkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -8,5 +9,7 @@ public class LessonsController {
     @Autowired
     private LessonService lessonService;
 
+    @Autowired
+    private MarkService markService;
 
 }
