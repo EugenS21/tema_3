c010dcdb7f3468a7cc1a20d4cbb486a6d9a695d9
vasilevieru <vasile_vieru@outlook.com>
2019-01-24 10:25:55 +0200

    get marks of students

--- src/main/java/com/marksmanagement/marksmanagement/controllers/MarkController.java
+++ src/main/java/com/marksmanagement/marksmanagement/controllers/MarkController.java
@@ -3,22 +3,37 @@ package com.marksmanagement.marksmanagement.controllers;
 import com.marksmanagement.marksmanagement.classes.Lesson;
 import com.marksmanagement.marksmanagement.classes.Mark;
 import com.marksmanagement.marksmanagement.classes.Student;
+import com.marksmanagement.marksmanagement.services.LessonService;
 import com.marksmanagement.marksmanagement.services.MarkService;
+import com.marksmanagement.marksmanagement.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MarkController {
     @Autowired
-    MarkService markService;
+    private MarkService markService;
 
-    @PostMapping(value="/inputMarks")
-    public String addMark(@ModelAttribute("newmark") Mark mark
-            , Errors error, Model model){
-        markService.addMark();
+    @Autowired
+    private StudentService studentService;
+
+    @Autowired
+    private LessonService lessonService;
+//    @PostMapping(value="/inputMarks")
+//    public String addMark(@ModelAttribute("newmark") Mark mark
+//            , Errors error, Model model){
+//        markService.addMark();
+//    }
+    @GetMapping(value = "/catalog")
+    public String getMarkOfStudentsToLessons(Model model){
+        model.addAttribute("marks", markService.getAllMarks());
+        model.addAttribute("students", studentService.getAllStudents());
+        model.addAttribute("lessons", lessonService.getAllLessons());
+        return "Catalog";
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,10 +4,12 @@ insert into Lessons (ID, Nume) values
 
 insert into Students (ID,Nume,Prenume) values
 (1,'Vieru','Vasile'),
+(3, 'Revenco', 'Alexandrina'),
 (2,'Savca','Eugen');
 
 insert into Marks (ID, Mark, ID_Student, ID_Lectie) values
 (1, 9, 1, 1),
 (2, 8, 1, 2),
 (3, 7, 2, 1),
-(4, 6, 2, 2);
+(4, 6, 2, 2),
+(5, 5, 3, 2);
